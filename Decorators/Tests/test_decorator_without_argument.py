2281b57f2adf57c32dd77ec6692c5fa8c7bb57f8
arvindadari <42448012+arvindadari21@users.noreply.github.com>
2018-10-28 11:32:04 +0530

    * Decortors without arguments

--- Decorators/Tests/test_decorator_without_argument.py
+++ Decorators/Tests/test_decorator_without_argument.py
@@ -3,7 +3,7 @@ Unittest cases for class based decorator
 """
 
 import unittest
-from Decorators.class_based_decorators import CustomDecorator
+from Decorators.decorators_without_arguments import custom_decorator
 
 
 class TestClassBasedDecorator(unittest.TestCase):
@@ -19,9 +19,9 @@ class TestClassBasedDecorator(unittest.TestCase):
 
     def test_custom_class_decorator(self):
 
-        def method_needs_decorator():
+        def method_without_argument_needs_decorator():
             print("This method needs class decorator")
 
-        # Call the method which is wrapped with decorator
-        method_needs_decorator = CustomDecorator()(method_needs_decorator)
-        method_needs_decorator()
+        # Call the method with decorator
+        method_without_argument_needs_decorator = custom_decorator(method_without_argument_needs_decorator)
+        method_without_argument_needs_decorator()
